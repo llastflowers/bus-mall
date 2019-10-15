a2cbdb02d6dcfb1c545f17fdbf96996ef472afe4
Brittany Houtz <brittany.houtz@gmail.com>
2019-10-14 23:29:48 -0700

    add user stories, HTML style, basic styling

---
+++ src/api.js
@@ -0,0 +1,183 @@
+ 
+const bag = {
+    id: 'bag',
+    name: 'R2-D2 Suitcase',
+    image: '../assets/bag.jpg',
+    description: 'A hard-sided suitcase with an R2-D2 graphic. Perfect for concealing the droids they are looking for.',
+    category: 'travel'
+};
+ 
+const banana = {
+    id: 'banana',
+    name: 'Banana Slicer',
+    image: '../assets/banana.jpg',
+    description: 'A banana slicer. It slices your bananas. How else would you slice your bananas?',
+    category: 'kitchen'
+};
+
+const bathroom = {
+    id: 'bathroom',
+    name: 'Bathroom Tablet Stand',
+    image: '../assets/bathroom.jpg',
+    description: 'A metal stand with a toilet paper holder and a tablet holder. All your favorite things in one place. Your favorite place!',
+    category: 'bathroom'
+};
+
+const boots = {
+    id: 'boots',
+    name: 'Open-Toed Rain Boots',
+    image: '../assets/boots.jpg',
+    description: 'Yellow rain boots in a trendy open-toe style. It totally makes sense if you think about it!',
+    category: 'apparel'
+};
+
+const breakfast = {
+    id: 'breakfast',
+    name: 'Multi-Purpose Breakfast Machine',
+    image: '../assets/breakfast.jpg',
+    description: 'A breakfast machine that cooks everything about halfway but all at once.',
+    category: 'kitchen'
+};
+
+const bubblegum = {
+    id: 'bubblegum',
+    name: 'Meatball Bubble Gum',
+    image: '../assets/bubblegum.jpg',
+    description: 'It\'s meatball bubble gum. Looks...delicious...',
+    category: 'edible'
+};
+
+const chair = {
+    id: 'chair',
+    name: 'Super-Ergonomic Chair',
+    image: '../assets/chair.jpg',
+    description: 'A really fancy chair. It\'s so comfortable. Promise.',
+    category: 'furniture'
+};
+
+const cthulhu = {
+    id: 'cthulhu',
+    name: 'Chthulhu Action Figure',
+    image: '../assets/cthulhu.jpg',
+    description: 'A Cathulhu action figure, and some hapless professor-type (doomed).',
+    category: 'toy'
+};
+
+const dogduck = {
+    id: 'dogduck',
+    name: 'Doggie Duckbill Muzzle',
+    image: '../assets/dog-duck.jpg',
+    description: 'A duck-bill shaped muzzle. Now YOU can lovingly humiliate your precious pup.',
+    category: 'pet'
+};
+
+const dragon = {
+    id: 'dragon',
+    name: 'Canned Dragon Meat',
+    image: '../assets/dragon.jpg',
+    description: 'A can of dragon meat. Farm-raised into late-adolescence, for 100 years.',
+    category: 'edible'
+};
+
+const pen = {
+    id: 'pen',
+    name: 'Flatwear Pens',
+    image: '../assets/pen.jpg',
+    description: 'Pens with flatware-shaped caps. You can write with them and also eat with them!',
+    category: 'office'
+};
+
+const petsweep = {
+    id: 'petsweep',
+    name: 'Pet Dust Boots',
+    image: '../assets/petsweep.jpg',
+    description: 'You pet can wear these dust boots around the house. They probably clean super well. Your pet will hate them!',
+    category: 'pet'
+};
+
+const scissors = {
+    id: 'scissors',
+    name: 'Pizza Scissors',
+    image: '../assets/scissors.jpg',
+    description: 'Okay actually, this is legit. You haven\'t lived \'til you\'ve cut a pizza with a giant pair of scissors.',
+    category: 'kitchen'
+};
+
+const shark = {
+    id: 'shark',
+    name: 'Shark Sleeping Bag',
+    image: '../assets/shark.jpg',
+    description: 'Ha ha ha, it looks like the shark is eating that guy! But it\'s not a real shark, it\'s just a sleeping bag shaped like a shark!',
+    category: 'bedding'
+};
+
+const sweep = {
+    id: 'sweep',
+    name: 'Baby Dust Mop Onesie',
+    image: '../assets/sweep.jpg',
+    description: 'Your baby can wear this onesie while he crawls around on your nasty dusty floor and then your baby will be covered in the nasty dust. Capital!',
+    category: 'apparel'
+};
+
+const tauntaun = {
+    id: 'tauntaun',
+    name: 'Tauntaun Sleeping Bag',
+    image: '../assets/tauntaun.jpg',
+    description: 'Perfect for the precocious child. You\'ll rest easy too, knowing your little one is safely nestled among the entrails of a snow lizard.',
+    category: 'bedding'
+};
+
+const unicorn = {
+    id: 'unicorn',
+    name: 'Can o\' Unicorn Meat',
+    image: '../assets/unicorn.jpg',
+    description: 'FANCY. This unicorn flesh will maybe definitely give you eternal life. But at what cost?',
+    category: 'food'
+};
+
+const usb = {
+    id: 'usb',
+    name: 'USB Tentacle Thingy',
+    image: '../assets/usb.jpg',
+    description: 'See, you plug this tentacle in to a USB port, and it WIGGLES. Why? Pffft. Because.',
+    category: 'gadget'
+};
+
+const watercan = {
+    id: 'watercan',
+    name: 'Never-Empty Watering Can',
+    image: '../assets/water-can.jpg',
+    description: 'Haha wait, I don\'t get it, \'cause it seems like if you tried to water a plant with this the water would just... Ohhhhhh!',
+    category: 'garden'
+};
+
+const wineglass = {
+    id: 'wineglass',
+    name: '~Aesthetic~ Wine Glass',
+    image: '../assets/wine-glass.jpg',
+    description: 'A most perplexing drinking vessel. Inconvenient, perhaps, but oh, the opulence!',
+    category: 'kitchen'
+};
+
+const testProductData = [
+    bag,
+    banana,
+    bathroom,
+    boots,
+    breakfast,
+    bubblegum,
+    chair,
+    cthulhu,
+    dogduck,
+    dragon,
+    pen,
+    petsweep,
+    scissors,
+    shark,
+    sweep,
+    tauntaun,
+    unicorn,
+    usb,
+    watercan,
+    wineglass
+];
--- user-stories.md
+++ user-stories.md
@@ -1,2 +1,9 @@
 USER STORIES
 
+1. As a marketing researcher, I want to see how often the same person chooses the same product repeatedly and which items are not selected at all most often, so I can zero in on the most and least popular items and thus determine the ideal number of items to feature in our catalog--we want enough variety, but not too many options.
+
+2. As a marketing researcher, I want to see how people respond to these materials so that I can predict which types of products are attractive to our demographic and minimize the cost of manufacturing and advertizing unpopular items.
+
+3. As a focus group participant, I want to view all of these new products because I am curious about interesting new items that are being invented and manufactured.
+
+4. As a focus group participant who rides on public transit often, I want to give my feedback on the items that may appear in the catalog because I may want to view the catalog in the future for entertainment or to make purchases.
